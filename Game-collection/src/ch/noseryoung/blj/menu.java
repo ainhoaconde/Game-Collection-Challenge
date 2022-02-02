0fb09a62e48b9be10ffd2c93294d436410b6cd17
MikkaK <mikka.kummer@edu.tbz.ch>
2022-02-02 14:30:26 +0100

    added menu

--- Game-collection/src/ch/noseryoung/blj/menu.java
+++ Game-collection/src/ch/noseryoung/blj/menu.java
@@ -2,11 +2,44 @@ package ch.noseryoung.blj;
 
 import java.util.Scanner;
 
-import static ch.noseryoung.blj.numberGuessing.numberGuessingGame;
 
 public class menu {
     public static void main(String[] args) {
-        numberGuessingGame();
+
+        Scanner myScanner = new Scanner(System.in); // Make scanner obj
+
+        System.out.println("\n" +
+                " '/¯¯¯¯|  |¯¯¯¯\\‚   |¯¯¯¯¯¯¯¯|   '/¯¯¯¯|   /¯¯¯¯|    |\\¯¯¯\\  |\\¯¯¯\\ °\n" +
+                " |        '\\/         |‚  |       ____|   |        '\\ '|       /|‘   | |     '|°| |     '|' \n" +
+                " |'\\     '|\\   /|     /'|'‚ |       ¯¯¯| |   |'\\     |\\  \\|      '|''|° °'/     '/  '/     /|‘ \n" +
+                " | '|   °'| '\\/ '|    '|°|'‚ |       ¯¯¯¯|   | '|   °|  \\       °|/    |       \\'/      | |' \n" +
+                " ''/___'\\  | /___'\\/'  |________|   ''/___|\\ /____¸|°    |\\_____/|__¸\\'  \n" +
+                " '|       |'\\|/|       |   |              |   '|      | '\\|        |     | |       '| |     |  \n" +
+                " '|____|  °|____|   |________|   '|__¸¸'|  '|____''|    °'\\|____'|/|___|‘ \n");
+
+        System.out.println("[1] - Rock Paper Scissors");
+        System.out.println("[2] - Hangman");
+        System.out.println("[3] - Number guessing game");
+
+        int choice = myScanner.nextInt();
+
+        if (choice == 1) {
+            //Rock Paper
+            rockPaper.rockPaperScissors();
+        }
+        else if (choice == 2){
+            //hangman
+
+        }
+        else if (choice == 3) {
+            //number
+            numberGuessing.numberGuessingGame();
+        }
+        else{
+            System.out.println("Invalid input");
+        }
+
+
     }
 
 }
