de7153ce3ae39be055c4ab0b75c441beeb13df38
ainhoaconde <90756914+ainhoaconde@users.noreply.github.com>
2022-02-02 12:12:49 +0100

    commit

--- Game-collection/src/ch/noseryoung/blj/menu.java
+++ Game-collection/src/ch/noseryoung/blj/menu.java
@@ -1,7 +1,42 @@
 package ch.noseryoung.blj;
+import java.util.Scanner;
 
 public class menu {
     public static void main(String[] args) {
 
-    }
-}
+                int secretNumber;
+
+                secretNumber = (int) (Math.random() * 99 + 1);
+
+                Scanner keyboard = new Scanner(System.in);
+
+                int guess;
+
+                do {
+
+                    System.out.print("Enter a guess (1-100): ");
+
+                    guess = keyboard.nextInt();
+
+                    if (guess == secretNumber)
+
+                        System.out.println("Your guess is correct. Congratulations:)");
+
+                    else if (guess < secretNumber)
+
+                        System.out
+
+                                .println("Your guess is smaller than the  number.");
+
+                    else if (guess > secretNumber)
+
+                        System.out
+
+                                .println("Your guess is bigger than the number.");
+
+                } while (guess != secretNumber);
+
+            }
+
+        }
+
--- Game-collection/src/ch/noseryoung/blj/rockPaper.java
+++ Game-collection/src/ch/noseryoung/blj/rockPaper.java
@@ -1,4 +1,88 @@
 package ch.noseryoung.blj;
+import java.util.Scanner;
 
 public class rockPaper {
-}
+
+        public static void rockPaper(String[] args) {
+            int scissors = 1;
+            int stone = 2;
+            int paper = 3;
+            int jack = 0;
+            int user = 0;
+            double jackchoice;
+            int userchoice;
+
+            Scanner aScanner = new Scanner (System.in);
+
+            do
+            {
+                jackchoice = (int) (Math.random()*3);
+
+                System.out.println("Jacke has now made a choice");
+                System.out.println("Now it is your turn to choose one \n 1 = scissors \n 2 = stone \n 3 = paper");
+
+                userchoice = aScanner.nextInt();
+
+                if ( userchoice == scissors)
+                {
+                    if (userchoice == jackchoice )
+                    {
+                        System.out.println(" You have the same choice as Jack.");
+                    }
+                    else if (jackchoice == stone)
+                    {
+                        jack++;
+                        System.out.println("The Jack won!");
+                    }
+                    else
+                    {
+                        user++;
+                        System.out.println(" You have won!");
+                    }
+
+                }
+
+                else if (userchoice == stone)
+                {
+                    if (userchoice == jackchoice )
+                    {
+                        System.out.println("You have the same choice as Jack.");
+                    }
+                    else if (jackchoice == paper)
+                    {
+                        jack++;
+                        System.out.println("The Jack won!");
+                    }
+                    else
+                    {
+                        user++;
+                        System.out.println("You have won!");
+                    }
+                }
+
+                else if (userchoice == paper)
+                {
+                    if (userchoice == jackchoice )
+                    {
+                        System.out.println("You have the same choice as the Jack.");
+                    }
+                    else if (jackchoice == scissors)
+                    {
+                        jack++;
+                        System.out.println("The Jack won!");
+                    }
+                    else
+                    {
+                        user++;
+                        System.out.println("You have won!");
+                    }
+                }
+
+                System.out.println("You have " + user + " points "+"Jack has " + jack + " points \n");
+            }
+            while ( (user != 3) && (jack != 3) );
+
+
+        }
+
+    }
