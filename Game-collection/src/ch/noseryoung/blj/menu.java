2f01b2c448da3286049c9be79063f2a354d39245
MikkaK <mikka.kummer@edu.tbz.ch>
2022-02-02 15:16:52 +0100

    changed ascii art

--- Game-collection/src/ch/noseryoung/blj/menu.java
+++ Game-collection/src/ch/noseryoung/blj/menu.java
@@ -2,44 +2,53 @@ package ch.noseryoung.blj;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 
 public class menu {
     public static void main(String[] args) {
-
         Scanner myScanner = new Scanner(System.in); // Make scanner obj
+        String choice;
+        int choiceI = 0;
+do {
+    System.out.println("\n" +
+            " .----------------.  .----------------.  .-----------------. .----------------. \n" +
+            "| .--------------. || .--------------. || .--------------. || .--------------. |\n" +
+            "| | ____    ____ | || |  _________   | || | ____  _____  | || | _____  _____ | |\n" +
+            "| ||_   \\  /   _|| || | |_   ___  |  | || ||_   \\|_   _| | || ||_   _||_   _|| |\n" +
+            "| |  |   \\/   |  | || |   | |_  \\_|  | || |  |   \\ | |   | || |  | |    | |  | |\n" +
+            "| |  | |\\  /| |  | || |   |  _|  _   | || |  | |\\ \\| |   | || |  | '    ' |  | |\n" +
+            "| | _| |_\\/_| |_ | || |  _| |___/ |  | || | _| |_\\   |_  | || |   \\ `--' /   | |\n" +
+            "| ||_____||_____|| || | |_________|  | || ||_____|\\____| | || |    `.__.'    | |\n" +
+            "| |              | || |              | || |              | || |              | |\n" +
+            "| '--------------' || '--------------' || '--------------' || '--------------' |\n" +
+            " '----------------'  '----------------'  '----------------'  '----------------' \n");
 
-        System.out.println("\n" +
-                " '/¯¯¯¯|  |¯¯¯¯\\‚   |¯¯¯¯¯¯¯¯|   '/¯¯¯¯|   /¯¯¯¯|    |\\¯¯¯\\  |\\¯¯¯\\ °\n" +
-                " |        '\\/         |‚  |       ____|   |        '\\ '|       /|‘   | |     '|°| |     '|' \n" +
-                " |'\\     '|\\   /|     /'|'‚ |       ¯¯¯| |   |'\\     |\\  \\|      '|''|° °'/     '/  '/     /|‘ \n" +
-                " | '|   °'| '\\/ '|    '|°|'‚ |       ¯¯¯¯|   | '|   °|  \\       °|/    |       \\'/      | |' \n" +
-                " ''/___'\\  | /___'\\/'  |________|   ''/___|\\ /____¸|°    |\\_____/|__¸\\'  \n" +
-                " '|       |'\\|/|       |   |              |   '|      | '\\|        |     | |       '| |     |  \n" +
-                " '|____|  °|____|   |________|   '|__¸¸'|  '|____''|    °'\\|____'|/|___|‘ \n");
+    System.out.println("[1] - Rock Paper Scissors");
+    System.out.println("[2] - Hangman");
+    System.out.println("[3] - Number guessing game");
+    System.out.println("[4] - Exit programm");
+    if (myScanner.hasNextLine()) {
+        choice = myScanner.nextLine();
+        choiceI = Integer.parseInt(choice);
+    }
+    if (choiceI == 1) {
+        //Rock Paper
+        rockPaper.rockPaperScissors();
+    } else if (choiceI == 2) {
+        //hangman
+        hangman.hangmangame();
+    } else if (choiceI == 3) {
+        //number
+        numberGuessing.numberGuessingGame();
+    }else if (choiceI == 4){
 
-        System.out.println("[1] - Rock Paper Scissors");
-        System.out.println("[2] - Hangman");
-        System.out.println("[3] - Number guessing game");
+    }
+    else {
+        System.out.println("Invalid input");
+    }
 
-        int choice = myScanner.nextInt();
-
-        if (choice == 1) {
-            //Rock Paper
-            rockPaper.rockPaperScissors();
-        }
-        else if (choice == 2){
-            //hangman
-            hangman.hangmangame();
-        }
-        else if (choice == 3) {
-            //number
-            numberGuessing.numberGuessingGame();
-        }
-        else{
-            System.out.println("Invalid input");
-        }
-
-
+}while (choiceI != 4);
     }
 
 }
--- Game-collection/src/ch/noseryoung/blj/numberGuessing.java
+++ Game-collection/src/ch/noseryoung/blj/numberGuessing.java
@@ -4,35 +4,22 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class numberGuessing {
-    public static void numberGuessingGame(){
-    // Getting Random Number
-    Random rand = new Random();
-    Scanner scan = new Scanner(System.in);
-    int n = rand.nextInt(11);
-    String num = String.valueOf(n);
-    //
-    int g = 0;
-    int gInput = 1;
-        System.out.println("Guess " + gInput + ":");
-    String i = scan.nextLine();
-        while(true){
-        g++;
-        gInput++;
-        if(g == 3){
-            System.out.println("You Lost!\nIt was " + num);
-            break;
-        }else{
-            if(i == num){
-                System.out.println("You Won!");
-                break;
-            }else{
-                System.out.println("Nope");
-                System.out.println("Guess " + gInput + ":");
-                i = scan.nextLine();
-
-            }
-        }
+    public static void numberGuessingGame() {
+        int secretNumber;
+        secretNumber = (int) (Math.random() * 99 + 1);
+        Scanner keyboard = new Scanner(System.in);
+        int guess;
+        do {
+            System.out.print("Enter a guess (1-100): ");
+            guess = keyboard.nextInt();
+            if (guess == secretNumber)
+                System.out.println("Your guess is correct. Congratulations:)");
+            else if (guess < secretNumber)
+                System.out
+                        .println("Your guess is smaller than the  number.");
+            else if (guess > secretNumber)
+                System.out
+                        .println("Your guess is bigger than the number.");
+        } while (guess != secretNumber);
     }
-    i = scan.nextLine();
-}
 }
